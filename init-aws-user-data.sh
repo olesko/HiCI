3ae8e08b9ae8595e03b2c75159b812c5cc15cde4
Oles Laba <olesko.li92@gmail.com>
2016-12-09 12:34:47 +0200

    added init aws user data script

---
+++ init-aws-user-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum -y update
+
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm"
+rpm -ivh jdk-8u45-linux-x64.rpm
+
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+yum -y install jenkins
+
+service jenkins start
+service jenkins status
+
+yum -y install cloud-utils
+
